b66944054400ec382c50613aef5181bec4a64488
Benedikt <53448898+TFox42@users.noreply.github.com>
2022-05-20 11:54:54 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AQM'
+copyright = '2022, HTL Villach'
+author = 'Benedikt Gruber'
 
 release = '0.1'
 version = '0.1.0'
